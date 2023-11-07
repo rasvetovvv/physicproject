f2787e4305fd93e45c5d5a14172037383ee0c2ed
rasvetovvv <116313906+rasvetovvv@users.noreply.github.com>
2023-11-08 00:19:22 +0200

    Add files via upload

---
+++ src/components/Logo.tsx
@@ -0,0 +1,53 @@
+export function Logomark(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" widths={36} height={36} viewBox="0 0 48 48" fill="none" {...props}>
+      <path d="M47.9978 21.92C47.9225 21.7223 45.4319 12.4154 37.0804 7.31133C35.4822 6.33571 30.0054 3.37803 22.8059 4.71052C21.1999 5.00834 12.7784 6.2022 9.67027 13.9558C9.44456 14.5181 6.6789 20.8391 10.4512 26.7596C14.1509 32.562 20.9171 33.2321 22.9667 33.4375C28.8171 34.0177 33.4534 32.0947 35.0801 31.1371C29.0273 35.3117 23.5531 35.9921 18.9298 34.942C-0.39351 30.5568 1.82733 2.42808 21.6021 1.00316C23.0731 0.897892 24.6453 0.928704 26.3239 1.11869C45.5356 3.29587 47.9614 21.6324 47.9978 21.92Z" fill="url(#paint0_linear_15410_10533)"/>
+      <path d="M48.0004 21.9246C48.0004 21.9246 48.0004 21.9246 48.0004 21.9221C47.9641 21.6345 45.5383 3.29795 26.3291 1.12077C24.6505 0.930779 23.0783 0.899967 21.6073 1.00523C1.83255 2.42759 -0.388277 30.5589 18.935 34.9441C23.5609 35.9941 29.0325 35.3138 35.0853 31.1391C34.4627 32.2483 33.7596 33.7399 31.8034 35.247C26.6119 39.5706 19.8664 39.2368 18.5484 39.1161C10.9442 38.4332 2.86773 32.1456 1.16579 23.8605C-0.162559 17.3854 3.04415 12.076 3.9029 10.6998C5.3013 8.46104 7.15632 6.51236 9.3045 4.89745C23.6724 -5.90885 44.5887 2.11436 47.7332 19.6987C47.9796 21.0774 48.0056 21.9221 48.0004 21.9246Z" fill="url(#paint1_linear_15410_10533)"/>
+      <path d="M0 26.0747C0.0752384 26.2724 2.56589 35.5793 10.9174 40.6833C12.5155 41.659 17.9924 44.6166 25.1919 43.2841C26.7978 42.9863 35.2194 41.7925 38.3275 34.0388C38.5532 33.4766 41.3189 27.1556 37.5466 21.2351C33.8469 15.4327 27.0806 14.7626 25.031 14.5572C19.1806 13.977 14.5444 15.9 12.9177 16.8576C18.9705 12.683 24.4447 12.0026 29.068 13.0527C48.3913 17.4379 46.1704 45.5666 26.3957 46.9915C24.9247 47.0968 23.3524 47.066 21.6739 46.876C2.46212 44.6988 0.036322 26.3622 0 26.0747Z" fill="url(#paint2_linear_15410_10533)"/>
+      <path d="M0.000567314 26.0748C0.000567314 26.0748 0.000567314 26.0748 0.000567314 26.0774C0.0368893 26.365 2.46267 44.7015 21.6718 46.8787C23.3504 47.0687 24.9226 47.0995 26.3937 46.9942C46.1684 45.5719 48.3892 17.4406 29.0659 13.0554C24.4401 12.0053 18.9684 12.6857 12.9156 16.8603C13.5383 15.7512 14.2414 14.2595 16.1976 12.7525C21.389 8.42891 28.1345 8.76268 29.4525 8.88335C37.0568 9.56628 45.1332 15.8539 46.8351 24.139C48.1635 30.614 44.9568 35.9235 44.098 37.2996C42.6996 39.5384 40.8446 41.4871 38.6964 43.102C24.3285 53.9083 3.41223 45.8851 0.267785 28.3008C0.0213145 26.9221 -0.00462154 26.0774 0.000567314 26.0748Z" fill="url(#paint3_linear_15410_10533)"/>
+      <defs>
+        <linearGradient id="paint0_linear_15410_10533" x1="5.54882" y1="18.162" x2="47.9983" y2="18.162" gradientUnits="userSpaceOnUse">
+          <stop offset="0.3752" stopColor="#C4DA4E"/>
+          <stop offset="1" stopColor="#D1E06D"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_15410_10533" x1="0.845947" y1="19.5876" x2="48.0004" y2="19.5876" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#D1E06E"/>
+          <stop offset="0.6248" stopColor="#C6DA44"/>
+        </linearGradient>
+        <linearGradient id="paint2_linear_15410_10533" x1="42.4489" y1="29.8327" x2="-0.000543989" y2="29.8327" gradientUnits="userSpaceOnUse">
+          <stop offset="0.3752" stopColor="#C4DA4E"/>
+          <stop offset="1" stopColor="#D1E06D"/>
+        </linearGradient>
+        <linearGradient id="paint3_linear_15410_10533" x1="47.1549" y1="28.4119" x2="0.000595851" y2="28.4119" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#D1E06E"/>
+          <stop offset="0.6248" stopColor="#C6DA44"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg aria-hidden="true" width={153} height={36} viewBox="0 0 136 32" fill="none" {...props}>
+      <defs>
+        <linearGradient id="paint0_linear_13600_9507" x1="3.69856" y1="12.109" x2="31.9982" y2="12.109" gradientUnits="userSpaceOnUse">
+          <stop offset="0.3752" stopColor="#C4DA4E"></stop>
+          <stop offset="1" stopColor="#D1E06D"></stop>
+        </linearGradient>
+        <linearGradient id="paint1_linear_13600_9507" x1="0.563313" y1="13.0584" x2="31.9996" y2="13.0584" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#D1E06E"></stop>
+          <stop offset="0.6248" stopColor="#C6DA44"></stop>
+        </linearGradient>
+        <linearGradient id="paint2_linear_13600_9507" x1="28.3012" y1="19.8896" x2="0.00159047" y2="19.8896" gradientUnits="userSpaceOnUse">
+          <stop offset="0.3752" stopColor="#C4DA4E"></stop>
+          <stop offset="1" stopColor="#D1E06D"></stop>
+        </linearGradient>
+        <linearGradient id="paint3_linear_13600_9507" x1="31.4366" y1="18.9414" x2="0.000397234" y2="18.9414" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#D1E06E"></stop>
+          <stop offset="0.6248" stopColor="#C6DA44"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
---
+++ src/lib/navigation.ts
@@ -0,0 +1,16 @@
+export const navigation = [
+  {
+    title: 'Початок',
+    links: [
+      { title: 'Розповідь', href: '/get-started/about' },
+    ],
+  },
+  {
+    title: 'Задачі',
+    links: [
+      { title: 'Перша', href: '/get-started/first' },
+      { title: 'Друга', href: '/get-started/two' },
+      { title: 'Третя', href: '/get-started/three' },
+    ],
+  }
+]
